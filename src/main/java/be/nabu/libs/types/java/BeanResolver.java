d47c607cb3af9657f44f2f5bb07e5d64b9ea30ac
alex <alex@nabu.be>
2014-11-07 12:21:18 +0100

    Fixed osgi Note: due to limited scope you might need to do BeanResolver.getInstance().register() for java classes in an OSGi environment...

--- src/main/java/be/nabu/libs/types/java/BeanResolver.java
+++ src/main/java/be/nabu/libs/types/java/BeanResolver.java
@@ -10,12 +10,21 @@ public class BeanResolver implements DefinedTypeResolver {
 
 	private Map<String, DefinedType> resolved = new HashMap<String, DefinedType>();
 	
+	private static BeanResolver instance;
+	
+	public static BeanResolver getInstance() {
+		if (instance == null) {
+			instance = new BeanResolver();
+		}
+		return instance;
+	}
+	
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	@Override
 	public DefinedType resolve(String id) {
 		if (!resolved.containsKey(id)) {
 			try {
-				Class<?> targetType = Class.forName(id);
+				Class<?> targetType = Thread.currentThread().getContextClassLoader().loadClass(id);
 				BeanType<?> beanType = new BeanType(targetType);
 				if (beanType.isSimpleType())
 					resolved.put(id, new SimpleBeanType(beanType));
@@ -27,5 +36,21 @@ public class BeanResolver implements DefinedTypeResolver {
 			}
 		}
 		return resolved.get(id);
+	}
+	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public void register(Class<?> clazz) {
+		if (!resolved.containsKey(clazz.getName())) {
+			resolved.put(clazz.getName(), new BeanType(clazz));
+		}
+	}
+	
+	@SuppressWarnings("unused")
+	private void activate() {
+		instance = this;
+	}
+	@SuppressWarnings("unused")
+	private void deactivate() {
+		instance = null;
 	}
 }

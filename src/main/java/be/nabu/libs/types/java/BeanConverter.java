dd94407a3a76ef2e8ef04d34902919f4c7409605
Alexander Verbruggen <alex@nabu.be>
2016-11-30 14:40:04 +0100

    Added a bean converter that can rewrap with a different type

---
+++ src/main/java/be/nabu/libs/types/java/BeanConverter.java
@@ -0,0 +1,34 @@
+package be.nabu.libs.types.java;
+
+import be.nabu.libs.types.api.TypeConverter;
+import be.nabu.libs.types.api.TypeInstance;
+
+public class BeanConverter implements TypeConverter {
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@Override
+	public <T> T convert(Object instance, TypeInstance from, TypeInstance to) {
+		if (instance == null || !(to.getType() instanceof BeanType)) {
+			return null;
+		}
+		Object original;
+		if (instance instanceof BeanInstance) {
+			original = ((BeanInstance) instance).getUnwrapped();
+		}
+		else {
+			original = instance;
+		}
+		if (((BeanType) to.getType()).getBeanClass().isAssignableFrom(original.getClass())) {
+			return (T) new BeanInstance((BeanType) to.getType(), original);
+		}
+		return null;
+	}
+
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@Override
+	public boolean canConvert(TypeInstance from, TypeInstance to) {
+		return from.getType() instanceof BeanType && to.getType() instanceof BeanType
+				&& ((BeanType) to).getBeanClass().isAssignableFrom(((BeanType) from).getBeanClass());
+	}
+
+}
